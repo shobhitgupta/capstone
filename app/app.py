273b17c9e62d77848aaead01446e3bfbdc1728d2
shobhit gupta <shobhitgupta1982@gmail.com>
2022-11-06 22:16:57 +0530

    Adding basic html page

---
+++ app/app.py
@@ -0,0 +1,22 @@
+# to open/create a new html file in the write mode
+f = open('udacity.html', 'w')
+
+# the html code which will go in the file GFG.html
+html_template = """<html>
+<head>
+<title>Udacity Capstone</title>
+</head>
+<body>
+<h2>Thanks Udacity Team</h2>
+  
+<p>This is a capstone project.</p>
+  
+</body>
+</html>
+"""
+
+# writing the code into the file
+f.write(html_template)
+
+# close the file
+f.close()
